937f86052e937cd0e30434d6dfa983528ce22613
bogdanvladmihai <v.bogdan19@yahoo.com>
2023-05-27 02:14:15 +0300

    first part of js added

--- quiz.js
+++ quiz.js
@@ -1,3 +1,6 @@
+let easyGeneral, hardGeneral, easyWho, hardWho, easyWhen, hardWhen, albums;
+const noQueries = 5;
+
 function createFiled(idName, fieldName, fieldRule) {
   form = document.getElementById("quiz");
   field = document.createElement("fieldset");
@@ -40,8 +43,6 @@ function createQuiz(firstName) {
   button.value = "Evaluează!";
   button.id = "submit";
 }
-
-let easyGeneral, hardGeneral, easyWho, hardWho, easyWhen, hardWhen, albums;
 
 function initQueries() {
   let url = "general.json";
@@ -136,14 +137,101 @@ function initQueries() {
   });
 }
 
+function generateRandomPerm() {
+  let arr = Array.from(Array(4).keys());
+  for (let i = 3; i >= 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
+
+function generateQueryOfFirstType(target, idName, shuffle) {
+  let N = target.length;
+  let pos = Math.floor(Math.random() * N);
+  let copy = {"q": target[pos].q, "c": target[pos].c, "w1": target[pos].w1, "w2": target[pos].w2, "w3": target[pos].w3};
+  let tmp = {"q": target[pos].q, "c": target[pos].c, "w1": target[pos].w1, "w2": target[pos].w2, "w3": target[pos].w3};
+  perm = generateRandomPerm();
+  if (shuffle) {
+    for (let i = 0; i < perm.length; i++) {
+      let b;
+      if (perm[i] == 0) {
+        b = tmp.c;
+      } else if (perm[i] == 1) {
+        b = tmp.w1;
+      } else if (perm[i] == 2) {
+        b = tmp.w2;
+      } else {
+        b = tmp.w3;
+      }
+      if (i == 0) {
+        copy.c = b;
+      } else if (i == 1) {
+        copy.w1 = b;
+      } else if (i == 2) {
+        copy.w2 = b;
+      } else {
+        copy.w3 = b;
+      }
+    }
+  }
+
+  query = document.createElement("fieldset");
+  query.id = idName + "_query_" + pos;
+  text = document.createElement("legend");
+  text.innerHTML = copy.q;
+  query.appendChild(text);
+  for (let i = 0; i < 4; i++) {
+    p = document.createElement("p");
+    input = document.createElement("input");
+    input.type = "radio";
+    input.name = query.id;
+    label = document.createElement("label");
+    if (i == 0) {
+      label.innerHTML = copy.c;
+    } else if (i == 1) {
+      label.innerHTML = copy.w1;
+    } else if (i == 2) {
+      label.innerHTML = copy.w2;
+    } else {
+      label.innerHTML = copy.w3;
+    }
+    p.appendChild(input);
+    p.appendChild(label);
+    query.appendChild(p);
+  }
+  document.getElementById(idName + "id").appendChild(query);
+}
+
+function generateQueryOfSecondType(target) {
+  
+}
+
 function populateQuiz(hardMode) {  
-  console.log(easyGeneral[0].q);
-  console.log(hardGeneral[0].q);
-  console.log(easyWho[0].q);
-  console.log(hardWho[0].q);
-  console.log(easyWhen[0].q);
-  console.log(hardWhen[0].q);
-  console.log(albums[0].album);
+  let easy = 4, hard = 1;
+  if (hardMode) {
+    easy = 2;
+    hard = 3;
+  } 
+  for (let i = 0; i < easy; i++) {
+    generateQueryOfFirstType(easyGeneral, "general", true);
+    generateQueryOfFirstType(easyWho, "who", false);
+  }
+  for (let i = 0; i < hard; i++) {
+    generateQueryOfFirstType(hardGeneral, "general", true);
+    generateQueryOfFirstType(hardWho, "who", false);
+  }
+
+  easy = 3;
+  if (hardMode) {
+    easy = 1;
+  }
+  for (let i = 0; i < easy; i++) {
+    generateQueryOfSecondType(easyWhen);
+  }
+  for (let i = 0; i < hard; i++) {
+    generateQueryOfSecondType(hardWhen);
+  }
 }
 
 window.onload = function() {
@@ -155,11 +243,21 @@ window.onload = function() {
     endFirstName = firstName.indexOf(" ");
     firstName = firstName.slice(0, endFirstName);
     createQuiz(firstName);
+    let color;
     if (document.getElementById("ez").checked) {
       populateQuiz(false);
+      color = "bisque";
     } else {
       populateQuiz(true);
+      color = "black";
     }
+    document.querySelectorAll('input[type="radio"]').forEach(function(item) {
+      item.style.accentColor = color;
+    })
+    document.querySelectorAll('input[type="range"]').forEach(function(item) {
+      item.style.backgorundColor = color;
+    })
+    console.log(color);
   });
 }
 
@@ -176,12 +274,16 @@ crearea și stergerea de elemente HTML:
   - remove in createQuiz
 folosirea a cel puțin unei metode din clasele: Math, Array, String, Date
   - String: in eventListener pentru submit la primul formlar, pentru a obtine prenumele
-
-
-modificarea stilului unui element sau al unui grup de elemente
-folosirea și modificarea evenimentelor generate de mouse si tastatură
-modificare de proprietăți
+  - Math: Math.Random(), Math.Floor() in generarea de intrebari
+  - Array: Array.from() - generez nuemrele de la 0 la N
 inputuri funcționale (de exemplu: input de tip text/range/number/radio/checkbox, select, textarea)
+  - functia de generare a query-urlor
+modificare de proprietăți
+  - modificarea proprietaatilor ale unor obiecte JSON in functiile de generare de query-uri
+modificarea stilului unui element sau al unui grup de elemente:
+  - modificarea culorii la radio-buttons si la range-uri in functie de dificultate
+
+folosirea și modificarea evenimentelor generate de mouse si tastatură
 folosirea setTimeout sau setInterval
 folosirea localStorage (să se pastreze în localStorage o colecție de elemente)
 folosirea a cel puțin unei metode din clasele: Math, Array, Date
